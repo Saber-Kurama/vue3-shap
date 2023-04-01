f74ef418c08873496b6209ec38f41ceed6b76f57
guochao <463561248@qq.com>
2023-04-01 12:01:37 +0800

    ✨ feat:  项目的初始化

---
+++ src/AdditiveForceVisualizer.tsx
@@ -0,0 +1,7 @@
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "AdditiveForceVisualizer",
+  setup() {
+    return <div>AdditiveForceVisualizer</div>;
+  },
+});
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+// import { withInstall } from "@dangojs/digitforce-ui-utils";
+import AdditiveForceVisualizer from "./AdditiveForceVisualizer";
+
+// export default withInstall(EditText);
+
+export default AdditiveForceVisualizer;
